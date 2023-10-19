4f9578be8b00a81f5d7e735d8c3728a9af4dd80e
Elena <egurnovaelena56@gmail.com>
2023-10-19 23:14:34 +0500

    Добавьте файлы проекта.

---
+++ tempppp/ClientWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tempppp
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientWindow.xaml
+    /// </summary>
+    /// 
+    public class Product
+    {
+        public string ProductArticleNumber { get; set; }
+        public string ProductName { get; set; } 
+        public string ProductDescription { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductPhoto {  get; set; }   
+        public string ProductManufacturer { get; set; }
+        public double ProductCost { get; set; }
+        public int ProductDiscountAmount { get; set; }
+        public int ProductQuantityInStock { get; set; }
+        public string ProductStatus { get; set;}
+    }
+
+
+    public partial class ClientWindow : Window
+    {
+        
+        public ClientWindow()
+        {
+            InitializeComponent();
+
+           
+        }
+       
+       
+
+        private async void cmdDeleteCar_Click(object sender, RoutedEventArgs e)
+        {
+          //  var product = ProductsDataGrid.SelectedItem as Product; 
+
+        }
+
+        private void cmdAddCar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void cmdGetCar_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(
+                @"Data Source=DESKTOP-Q54H48I\SQLEXPRESS;Initial Catalog=Trade; Integrated Security=True"
+                );
+
+            using (conn)
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT ProductArticleNumber, ProductName FROM Product", conn);
+                SqlDataReader read = cmd.ExecuteReader();
+
+                using (read)
+                {
+                    dt.Load(read);
+                }
+
+
+            }
+
+            ProductsDataGrid.Items.Clear();
+            ProductsDataGrid.ItemsSource = dt.DefaultView;
+
+            //ProductsDataGrid.Columns.Add(new DataGridTextColumn
+            //{
+            //    Header = "ProductName",
+            //    Binding = new Binding("ProductName")
+            //});
+
+
+            conn.Close();
+            conn.Dispose();
+
+
+
+
+
+        }
+
+       
+    }
+}
+
---
+++ tempppp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tempppp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=DESKTOP-Q54H48I\SQLEXPRESS;Initial Catalog=Trade; Integrated Security=True");
+            string query = "Select * from [User] where UserLogin = '" + loginTB.Text.Trim() + "' and UserPassword = '" + passTB.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            sqlcon.Open();
+            SqlDataReader myReader = null;
+            SqlCommand command = new SqlCommand("Select * from [User] Where UserLogin = '" + loginTB.Text.Trim() + " ' and UserPassword = '" + passTB.Text.Trim() + "'", sqlcon);
+            myReader = command.ExecuteReader();
+            if (dtbl.Rows.Count == 0)
+            {
+                MessageBox.Show("Неправильно введен логин или пароль!", "Ошибка!");
+                ErrorWindow errorWin = new ErrorWindow();
+                errorWin.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Выполнен вход!", "Успешно!");
+                MainWWW mainwww = new MainWWW();
+                mainwww.Show();
+                this.Hide();
+            }
+        }
+    }
+}
